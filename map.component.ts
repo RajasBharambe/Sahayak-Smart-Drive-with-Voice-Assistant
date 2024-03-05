0e09eecb17c2b964ba78d8948d1fc16a4695d650
Bhavikakolhe <102041413+Bhavikakolhe@users.noreply.github.com>
2024-03-05 18:37:13 +0530

    Files for Map UI
    I have added the root files to route the MAP component.
    Index.html contains the OpenstreetMap API.
    You will need to create a component called map and then copy paste this code 
    steps to generate component:
    
    Open project
    open terminal 
    type----- map.component.ts
    select CSS
    Select N for routing

---
+++ map.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
+
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MapComponent implements OnInit {
+  map!: Map;
+
+  ngOnInit() {
+    this.createMap();
+  }
+  
+  createMap() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([73.8567, 18.5204]),
+        zoom: 12,
+      }),
+    });
+  }  
+  
+}
+
